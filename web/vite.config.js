f7133443405d9cc2a9a22aa4936e3714cf4229a5
Jona Cwejman <jcwejman@gmail.com>
2024-02-26 10:34:19 +0100

    chore(web): Ehance prettifying + lint & add tailwind setup
    Make sure that the headache of reading tailwind classnames are reduced
    with good prettifying support
    
    lint fixup

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,3 +1,4 @@
+import path from 'node:path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -5,5 +6,10 @@ export default defineConfig({
   plugins: [react()],
   build: {
     minify: false,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 });
