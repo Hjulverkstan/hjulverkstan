2c2f51fef5633b0af871737042d72b15648471cf
Jona Cwejman <jcwejman@gmail.com>
2024-03-12 18:10:26 +0100

    feat(api): Enable CORS in dev

---
+++ main/src/main/java/se/hjulverkstan/main/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package se.hjulverkstan.main.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.beans.factory.annotation.Value;
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig {
+
+    @Value("${cors.allowedOrigins}")
+    private String[] allowedOrigins;
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList(allowedOrigins));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config); // Apply CORS configuration to all paths
+        return new CorsFilter(source);
+    }
+}
