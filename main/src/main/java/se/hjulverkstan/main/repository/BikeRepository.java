c0a990d12eec02bae82361769782e2c253454cd7
SamuelSiesjo <samuel@siesjo.se>
2024-05-15 12:21:32 +0200

    chore(api): Remove unused Bike entity

--- main/src/main/java/se/hjulverkstan/main/repository/BikeRepository.java
+++
@@ -1,9 +0,0 @@
-package se.hjulverkstan.main.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import se.hjulverkstan.main.model.Bike;
-
-@Repository
-public interface BikeRepository extends JpaRepository<Bike, Long> {
-}
