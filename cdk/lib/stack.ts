a6f38a08194d88bc7150ebcc0a48b74c81756434
kalagarsamy <krishna-raja.alagarsamy@alten.se>
2024-05-22 09:09:12 +0200

    feat(cdk): Create cloudfront and route53, then using certificate also.

--- cdk/lib/stack.ts
+++ cdk/lib/stack.ts
@@ -1,11 +1,10 @@
 import * as cdk from 'aws-cdk-lib';
-import {
-  InstanceClass,
-  InstanceSize,
-  InstanceType,
-  SubnetType,
-  Vpc,
-} from "aws-cdk-lib/aws-ec2";
+import { InstanceClass, InstanceSize, InstanceType, SubnetType, Vpc } from "aws-cdk-lib/aws-ec2";
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as route53 from 'aws-cdk-lib/aws-route53';
+import * as targets from 'aws-cdk-lib/aws-route53-targets';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as ecs_patterns from 'aws-cdk-lib/aws-ecs-patterns';
@@ -27,39 +26,63 @@ export class Stack extends cdk.Stack {
       vpc: vpc
     });
 
+    // Create an S3 bucket
+    const bucket = new s3.Bucket(this, 'ReactS3Bucket', {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      encryption: s3.BucketEncryption.KMS_MANAGED,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      bucketName: 'stack-s3',
+    });
+
+    // Import existing hosted zone
+    const hostedZoneId = 'Z06587302B0UA1QW934WE';
+    const zoneName = 'hjulverkstan.org';
+    const hostedZone = route53.HostedZone.fromHostedZoneAttributes(this, 'ImportedHostedZone', {
+      hostedZoneId: hostedZoneId,
+      zoneName: zoneName
+    });
+
+    // Import an existing certificate
+    const certificateArn = 'arn:aws:acm:us-east-1:730335549373:certificate/ea2780e0-0343-4b48-902c-3588aa19c016';
+    const certificate = acm.Certificate.fromCertificateArn(this, 'ImportedCertificate', certificateArn);
+
+    // Create CloudFront distribution
+    const distribution = new cloudfront.Distribution(this, 'MyDistribution', {
+      defaultBehavior: { origin: new origins.S3Origin(bucket) },
+      domainNames: ['hjulverkstan.org'],
+      certificate: certificate
+    });
+
+    // Create Route 53 alias record to CloudFront distribution
+    new route53.ARecord(this, 'AliasRecord', {
+      zone: hostedZone,
+      recordName: 'hjulverkstan.org',
+      target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution)),
+    });
+
     // Define database credentials
     const dbUsername = 'postgres';
     const dbPassword = cdk.SecretValue.unsafePlainText('secretpass');
     // Create RDS instance
     new DatabaseInstance(this, 'PostgresDB', {
-    engine,
-    instanceType,
-    vpc,
-    vpcSubnets: { subnetType: SubnetType.PRIVATE_WITH_EGRESS}, // Use private subnets with NAT Gateway
-    credentials: Credentials.fromUsername(dbUsername, { password: dbPassword }),
-    removalPolicy: cdk.RemovalPolicy.DESTROY, // Destroy RDS instance when stack is deleted (for testing purposes)
-  });
+      engine,
+      instanceType,
+      vpc,
+      vpcSubnets: { subnetType: SubnetType.PRIVATE_WITH_EGRESS },
+      credentials: Credentials.fromUsername(dbUsername, { password: dbPassword }),
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
 
-  // Create an S3 bucket
-  new s3.Bucket(this, 'ReactS3Bucket', {
-    versioned: true, // Enable versioning
-    removalPolicy: cdk.RemovalPolicy.DESTROY, // This will delete the bucket when the stack is deleted
-    autoDeleteObjects: true, // This will delete objects when the bucket is deleted
-    encryption: s3.BucketEncryption.KMS_MANAGED, // Use KMS encryption managed by AWS Key Management Service
-    blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL, // Block public access to the bucket
-    bucketName: 'Stack-S3', // Optional: specify a bucket name
-    // Add other bucket configurations as needed
-  });
-  
-
-  new ecs_patterns.ApplicationLoadBalancedFargateService(this, "FargateService", {
-    cluster: cluster,
-    cpu: 256,
-    desiredCount: 6,
-    taskImageOptions: {image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample")},
-    memoryLimitMiB: 2048,
-    publicLoadBalancer: true
-  });
+    new ecs_patterns.ApplicationLoadBalancedFargateService(this, "FargateService", {
+      cluster: cluster,
+      cpu: 256,
+      desiredCount: 6,
+      taskImageOptions: { image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample") },
+      memoryLimitMiB: 2048,
+      publicLoadBalancer: true
+    });
 
   }
 }
