abf8bd938294837ae5aa5bc9e8b5858f18b2e14f
daniel-svendsen <daniel-svendsen@hotmail.se>
2024-09-05 10:17:50 +0200

    feat(web): Help popover on portal searchbar
    issue: #19

--- web/src/components/DataTable/DataTableFilterSearch.tsx
+++ web/src/components/DataTable/DataTableFilterSearch.tsx
@@ -1,10 +1,17 @@
 import { useState, useEffect } from 'react';
+import { InfoCircledIcon } from '@radix-ui/react-icons';
 
 import * as U from '@utils';
 import { Input } from '@components/shadcn/Input';
 
-import { Row, useDataTable } from './';
 import { buttonVariants } from '@components/shadcn/Button';
+import {
+  Content as PopoverContent,
+  Root as PopoverRoot,
+  Trigger as PopoverTrigger,
+} from '@components/shadcn/Popover';
+
+import { Row, useDataTable } from './';
 
 //
 
@@ -29,23 +36,51 @@ export const FilterSearch = ({ placeholder, matchFn }: FilterSearchProps) => {
   }, [isFiltered]);
 
   return (
-    <Input
-      disabled={disabled}
-      placeholder={placeholder}
-      value={value}
-      onChange={({ target: { value } }) => {
-        const filterFn = (row: Row) =>
-          value?.split(' ').every((word) => matchFn(word.toLowerCase(), row));
+    <div className="relative flex items-center">
+      <div>
+        <Input
+          disabled={disabled}
+          placeholder={placeholder}
+          value={value}
+          onChange={({ target: { value } }) => {
+            const filterFn = (row: Row) =>
+              value
+                ?.split(' ')
+                .every((word) => matchFn(word.toLowerCase(), row));
 
-        setValue(value);
-        setFilterFn('ANY', !!value && filterFn);
-      }}
-      data-state={!!value && 'active'}
-      className={U.cn(
-        buttonVariants({ variant: 'accent', subVariant: 'flat' }),
-        'h-8 w-[150px] font-normal data-[state=active]:font-medium lg:w-[250px]',
-      )}
-    />
+            setValue(value);
+            setFilterFn('ANY', !!value && filterFn);
+          }}
+          data-state={!!value && 'active'}
+          className={U.cn(
+            buttonVariants({ variant: 'accent', subVariant: 'flat' }),
+            `h-8 w-[150px] font-normal data-[state=active]:font-medium
+            lg:w-[250px]`,
+          )}
+        />
+        <PopoverRoot>
+          <PopoverTrigger asChild>
+            <span
+              className="hover:bg-muted text-muted-foreground
+                hover:text-accent-foreground absolute right-2 top-1/2 z-10 flex
+                h-6 w-6 -translate-y-1/2 transform cursor-pointer items-center
+                justify-center rounded-sm text-sm text-black"
+            >
+              <InfoCircledIcon className="h-4 w-4" />
+            </span>
+          </PopoverTrigger>
+          <PopoverContent
+            className="bg-muted text-accent-foreground"
+            sideOffset={12}
+          >
+            <p className="text-sm">
+              Search across all columns, combine multiple keywords to refine
+              your query.
+            </p>
+          </PopoverContent>
+        </PopoverRoot>
+      </div>
+    </div>
   );
 };
 
