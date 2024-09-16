335900c953cc476c8fe985f5e656bbca4c5bbb41
Adam PÄÄJÄRVI <adam.paajarvi@alten.se>
2024-09-16 14:29:32 +0200

    feat(web): Persisting Portal Filters
    Filters for the Portal were persisted. This is done so that
    users of the portal don't have to get annoyed when they switch tabs or
    which section they are working in. This was done by changing from
    useState to usePersistentState + useSlugs (that was edited). Within
    FilterSearch onChange was simplified and the bit of code within it was
    moved to a useEffect outside it for a proper persistent search bar.

--- web/src/hooks/useSlugs.ts
+++ web/src/hooks/useSlugs.ts
@@ -3,21 +3,23 @@ import { useLocation } from 'react-router-dom';
 export default function usePortalSlugs(): {
   url: string;
   baseUrl: string;
-  coreUrl?: string;
-  appSlug?: string;
+  coreUrl: string;
+  appSlug: string;
+  pageSlug: string;
   tailSlug?: string;
 } {
   const location = useLocation();
-  const match = location.pathname.match(/^(\/[^/]+)(?:(\/[^/]+))?(\/.*)?/);
+  const match = location.pathname.match(/^(\/[^/]+)(\/[^/]+)(\/[^/]+)?(\/.*)?/);
 
   if (!match) throw new Error('useSlugs failed to match the location pathname');
 
-  const [url, baseUrl, appSlug, tailSlug] = match;
+  const [url, baseUrl, appSlug, pageSlug, tailSlug] = match;
 
   return {
     url,
     baseUrl,
     appSlug,
+    pageSlug,
     tailSlug,
     coreUrl: baseUrl + appSlug,
   };
