10cb5f9b1f6a7810dc32e2405482fe60f7f9faf4
Adam <adam.paajarvi@alten.se>
2025-02-17 09:12:57 +0100

    fix(api): Enables rendering on Admin/Users
    Added logic for when a User is created or edited.
    It is critical that users can see this in our design
    and for the backend to handle the data correctly.
    Added Auditable to Users and other minor changes in
    other files to accomodate createdAt and editedAt.
    
    Solves issue #48.

--- main/src/main/java/se/hjulverkstan/main/dto/user/SignupRequest.java
+++ main/src/main/java/se/hjulverkstan/main/dto/user/SignupRequest.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 import se.hjulverkstan.main.model.ERole;
 
+import java.time.LocalDateTime;
 import java.util.Set;
 
 @Data
--- main/src/main/resources/data.sql
+++ main/src/main/resources/data.sql
@@ -122,10 +122,10 @@ INSERT INTO Localised_Content
 --
 
 INSERT INTO users
-(id, username,       email,                     password                                                      ) VALUES
-(1,  'christopher',  'admin@example.com',       '$2a$10$OV/brazFuYRnDqmaNKNereIvy8VK0RzZOw1ptctgw4fJLRCMckRfO'),
-(2,  'user',         'user@example.com',        '$2a$10$OV/brazFuYRnDqmaNKNereIvy8VK0RzZOw1ptctgw4fJLRCMckRfO'),
-(3,  'admin',        'admin2@example.com',      '$2a$10$OV/brazFuYRnDqmaNKNereIvy8VK0RzZOw1ptctgw4fJLRCMckRfO');
+(id,    username,       email,                     password, created_at, updated_at, created_by, updated_by                                                      ) VALUES
+(1,  'christopher',  'admin@example.com',       '$2a$10$OV/brazFuYRnDqmaNKNereIvy8VK0RzZOw1ptctgw4fJLRCMckRfO', '2024-05-16 10:00:00', NOW(), 1, 2),
+(2,  'user',         'user@example.com',        '$2a$10$OV/brazFuYRnDqmaNKNereIvy8VK0RzZOw1ptctgw4fJLRCMckRfO', '2024-07-17 10:00:00', NOW(), 1, 2),
+(3,  'admin',        'admin2@example.com',      '$2a$10$OV/brazFuYRnDqmaNKNereIvy8VK0RzZOw1ptctgw4fJLRCMckRfO', '2024-10-18 10:00:00', NOW(), 1, 2);
 
 INSERT INTO roles
 (id, name       ) VALUES
