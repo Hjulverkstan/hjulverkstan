eec309735c8a5461bb0f5cd25b7d2a949487e3dc
Adam <adam.paajarvi@alten.se>
2025-03-26 15:37:13 +0100

    refactor(api): Replaced UserResponse with UserDto
    Code surrounding user was convoluted and hard to maintain. If
    new fields were ever added to User, then changes to the code
    would have had to be made in numerous locations. To fix this
    and to make the code more familiar it was changed to resemble
    other sections of the code.

--- main/src/main/java/se/hjulverkstan/main/dto/responses/GetAllUserDto.java
+++ main/src/main/java/se/hjulverkstan/main/dto/responses/GetAllUserDto.java
@@ -4,14 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import se.hjulverkstan.main.dto.user.UserDto;
-import se.hjulverkstan.main.dto.user.UserResponse;
 
 import java.util.List;
-
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class GetAllUserDto {
-    private List<UserResponse> users;
+    private List<UserDto> users;
 }
