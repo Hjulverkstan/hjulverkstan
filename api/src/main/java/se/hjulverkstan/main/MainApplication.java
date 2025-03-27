440ec7562119732ec7e4dc696f32f3309cb44eda
Adam <adam.paajarvi@alten.se>
2025-03-27 13:58:18 +0100

    feat(api): S3 Orphan File Cleaner
    New class is created to clean (delete) orphaned file (image) keys
    from the S3 bucket. Orphaned files are those that are not
    referenced by any objects.
    
    Solves issue #147

--- api/src/main/java/se/hjulverkstan/main/MainApplication.java
+++ api/src/main/java/se/hjulverkstan/main/MainApplication.java
@@ -3,8 +3,10 @@ package se.hjulverkstan.main;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import se.hjulverkstan.main.config.DotenvApplicationContextInitializer;
 
+@EnableScheduling
 @SpringBootApplication
 public class MainApplication {
 	public static void main(String[] args) {
--- api/src/main/java/se/hjulverkstan/main/service/S3ServiceImpl.java
+++ api/src/main/java/se/hjulverkstan/main/service/S3ServiceImpl.java
@@ -2,10 +2,7 @@ package se.hjulverkstan.main.service;
 
 import com.amazonaws.SdkClientException;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.DeleteObjectRequest;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.*;
 import org.apache.tika.Tika;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -14,19 +11,16 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import se.hjulverkstan.Exceptions.*;
 import se.hjulverkstan.main.dto.ImageUploadResponse;
-import se.hjulverkstan.main.repository.ImageRepository;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Arrays;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
 public class S3ServiceImpl implements S3Service {
-
     private final AmazonS3 amazonS3;
     private static final List<String> ALLOWED_MIME_TYPES = Arrays.asList("image/jpeg", "image/png", "image/gif");
     private static final Tika TIKA = new Tika();
@@ -118,6 +112,35 @@ public class S3ServiceImpl implements S3Service {
         } catch (Exception e) {
             throw new S3DeleteException("Error deleting file from S3: " + e.getMessage());
         }
+    }
+
+    @Override
+    public void deleteFilesByKeys(List<String> keys) {
+        try {
+            DeleteObjectsRequest deleteObjectsRequest = new DeleteObjectsRequest(bucketName).withKeys(keys.toArray(new String[0]));
+            amazonS3.deleteObjects(deleteObjectsRequest);
+        } catch (Exception e) {
+            throw new S3DeleteException("Error deleting files from S3: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Retrieves all imageKeys (file names) stored in the S3 bucket.
+     *
+     * This method sends a request to list all objects in the specified S3 bucket
+     * and extracts their unique file keys. These keys represent the stored files
+     * without the full S3 URL.
+     *
+     * @return A List of Strings containing the file keys of all objects in the bucket.
+     */
+    @Override
+    public List<String> getAllImageKeys() {
+        ListObjectsV2Request request = new ListObjectsV2Request().withBucketName(bucketName);
+        ListObjectsV2Result result = amazonS3.listObjectsV2(request);
+
+        return result.getObjectSummaries().stream()
+                .map(S3ObjectSummary::getKey)
+                .collect(Collectors.toList());
     }
 
 }
