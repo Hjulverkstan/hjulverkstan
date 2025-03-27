33b7e0fd59a822e169e08af51026da11d43de4f1
Adam <adam.paajarvi@alten.se>
2025-03-27 13:58:18 +0100

    feat(api): S3 Orphan File Cleaner
    New class is created to clean (delete) orphaned file (image) keys
    from the S3 bucket. Orphaned files are those that are not
    referenced by any objects.
    
    Solves issue #147

--- api/src/main/java/se/hjulverkstan/main/service/S3Service.java
+++ api/src/main/java/se/hjulverkstan/main/service/S3Service.java
@@ -3,11 +3,14 @@ package se.hjulverkstan.main.service;
 import org.springframework.web.multipart.MultipartFile;
 import se.hjulverkstan.main.dto.ImageUploadResponse;
 
-public interface S3Service {
+import java.util.List;
 
+public interface S3Service {
     ImageUploadResponse uploadFile(MultipartFile file);
 
     String extractKeyFromURL(String url);
     void deleteFileByKey(String key);
+    void deleteFilesByKeys(List<String> keys);
 
+    List<String> getAllImageKeys();
 }
