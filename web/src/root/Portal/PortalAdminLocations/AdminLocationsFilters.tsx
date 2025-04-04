b08128f2e777b2ffa81107de28851b4eb11eebc5
Adam <adam.paajarvi@alten.se>
2025-04-04 12:58:26 +0200

    fix(web): Searching for vehicleCount, createdAt/updatedAt in Location
    Resolves #194

--- web/src/root/Portal/PortalAdminLocations/AdminLocationsFilters.tsx
+++ web/src/root/Portal/PortalAdminLocations/AdminLocationsFilters.tsx
@@ -3,15 +3,20 @@ import * as enums from '@data/location/enums';
 import * as DataTable from '@components/DataTable';
 import { Location } from '@data/location/types';
 import { PortalFilterDate } from '../PortalFilterDate';
+import { matchDateWithoutTimestamp } from '@utils';
 
 export default function AdminLocationFilters() {
   return (
     <>
       <DataTable.FilterSearch
         placeholder="Search in Locations..."
-        matchFn={(word: string, row: Location) =>
-          enums.matchFn(word, row) ||
-          DataTable.fuzzyMatchFn(['comment', 'name', 'address'], word, row)
+        matchFn={
+          (word: string, row: Location) =>
+            matchDateWithoutTimestamp(word, row.createdAt) ||
+            matchDateWithoutTimestamp(word, row.updatedAt) ||
+            enums.matchFn(word, row) ||
+            DataTable.fuzzyMatchFn(['comment', 'name', 'address'], word, row) ||
+            row.vehicleIds.length.toString().includes(word) // vehicleCount
         }
       />
 
