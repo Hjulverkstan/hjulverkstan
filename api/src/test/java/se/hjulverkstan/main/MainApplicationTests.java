3c6f642a3f648a54209bcd8843bed01649586485
cwejman <jcwejman@gmail.com>
2025-04-26 15:04:14 +0200

    revert(api): Context init of env vars not working – patch not needed
    For some reason teh post processor was not needed and the context initialiser now just works again in th e pipeline while the post processor does not.
    
    Not sure what happenbed before but revertin to how it was beofre and now the pipelines are happy again

--- api/src/test/java/se/hjulverkstan/main/MainApplicationTests.java
+++ api/src/test/java/se/hjulverkstan/main/MainApplicationTests.java
@@ -2,8 +2,11 @@ package se.hjulverkstan.main;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import se.hjulverkstan.main.config.DotenvApplicationContextInitializer;
 
 @SpringBootTest
+@ContextConfiguration(initializers = DotenvApplicationContextInitializer.class)
 class MainApplicationTests {
 	@Test
 	void contextLoads() {
