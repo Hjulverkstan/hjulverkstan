1947f8d0dda5b3ed436d838d1c571b40f0fd8103
cwejman <jcwejman@gmail.com>
2025-04-26 13:33:46 +0200

    fix(api): Context init of env vars not working in pipeline

--- api/src/test/java/se/hjulverkstan/main/MainApplicationTests.java
+++ api/src/test/java/se/hjulverkstan/main/MainApplicationTests.java
@@ -2,11 +2,8 @@ package se.hjulverkstan.main;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-import se.hjulverkstan.main.config.DotenvApplicationContextInitializer;
 
 @SpringBootTest
-@ContextConfiguration(initializers = DotenvApplicationContextInitializer.class)
 class MainApplicationTests {
 	@Test
 	void contextLoads() {
