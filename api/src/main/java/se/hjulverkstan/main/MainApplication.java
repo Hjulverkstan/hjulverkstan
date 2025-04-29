33da6a2a2e4851e601ac4fdfbf0a65a520a81d61
Jimmy Emanuelsson <jimmyem@gmail.com>
2025-04-29 10:43:35 +0200

    fix(api): Revert context initializer missing one file

--- api/src/main/java/se/hjulverkstan/main/MainApplication.java
+++ api/src/main/java/se/hjulverkstan/main/MainApplication.java
@@ -3,11 +3,14 @@ package se.hjulverkstan.main;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import se.hjulverkstan.main.config.DotenvApplicationContextInitializer;
 
 @SpringBootApplication
 public class MainApplication {
 	public static void main(String[] args) {
 		SpringApplication application = new SpringApplication(MainApplication.class);
+
+		application.addInitializers(new DotenvApplicationContextInitializer());
 		application.run(args);
 	}
 }
