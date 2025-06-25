5325a99d90845fbde7b2027692618cc17ccb370d
karlojoseph <karlo.joseph.valencia@gmail.com>
2025-06-25 10:07:26 +0200

    test(api): Brute force test using python
    -Added a pytest to perform brute force based on the common credentials found in the internet; ensuring that the correct authentication logic is in place.
    -Modifying dev web script to properly load the .env file during local run.
    -Updated .env template with new variables for pen testing

---
+++ api/test/python/brute_force_test/brute_force_test.py
@@ -0,0 +1,45 @@
+import requests
+import pytest
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Access the variables
+api_base_url = os.getenv('TEST_API_URL')
+app_users_username = os.getenv('TEST_SUCCESSFUL_USERS')
+app_users_password = os.getenv('TEST_SUCCESSFUL_PASSWORDS')
+
+API_URL = f"{api_base_url}/auth/login"
+
+def load_list(filename):
+    with open(filename, "r") as f:
+        return [line.strip() for line in f if line.strip()]
+
+# Load brute force sample usernames and passwords from files
+usernames = load_list(os.path.join(os.path.dirname(__file__), "../resources/usernames.txt"))
+passwords = load_list(os.path.join(os.path.dirname(__file__), "../resources/passwords.txt"))
+
+
+# Define the expected working combinations from environment variables
+valid_combinations = list(zip(
+    app_users_username.split(","),
+    app_users_password.split(",")
+))
+
+@pytest.mark.parametrize("username", usernames)
+@pytest.mark.parametrize("password", passwords)
+def test_brute_force_login(username, password):
+    payload = {"username": username, "password": password}
+    try:
+        response = requests.post(API_URL, json=payload)
+        access_token = response.cookies.get("accessToken")
+        print(access_token)
+        is_success = response.status_code == 200 and ("id" in response.json() or "accessToken" == access_token)
+        if (username, password) in valid_combinations:
+            assert is_success, f"Expected SUCCESS for {username}/{password}, but got {response.status_code} and {response.text}"
+        else:
+            assert not is_success, f"Unexpected SUCCESS for {username}/{password}"
+    except Exception as e:
+        pytest.fail(f"Exception for {username}/{password}: {e}")
