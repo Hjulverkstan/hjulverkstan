4bf56a6bcadcfc548b849112cd2d600794363342
cwejman <jcwejman@gmail.com>
2026-01-09 11:48:25 +0100

    feat: Remove start/end date from repair ticket

--- web/src/data/ticket/form.ts
+++ web/src/data/ticket/form.ts
@@ -1,22 +1,14 @@
 import { z } from 'zod';
-import { isReq, reqString } from '../form';
+
 import { Ticket, TicketType } from '@data/ticket/types';
+
+import { isReq, reqString } from '../form';
 
 export const initTicket = {
   vehicleIds: [],
 } as Partial<Ticket>;
 
 const ticketBaseZ = z.object({
-  ticketType: z.nativeEnum(TicketType, isReq('Ticket type')),
-  startDate: z.string(isReq('Start date')),
-  vehicleIds: z
-    .array(z.string(isReq('Vehicle ids')))
-    .min(1, 'At least one vehicle is required'),
-  employeeId: z.string(isReq('Employee id')),
-  customerId: z.string(isReq('Customer id')),
-});
-
-const ticketBaseZSecond = z.object({
   ticketType: z.nativeEnum(TicketType, isReq('Ticket type')),
   vehicleIds: z
     .array(z.string(isReq('Vehicle ids')))
@@ -30,17 +22,17 @@ export const ticketZ = z.discriminatedUnion(
   [
     ticketBaseZ.extend({
       ticketType: z.literal(TicketType.RENT),
+      startDate: z.string(isReq('Start date')),
       endDate: z.string(isReq('End date')),
     }),
     ticketBaseZ.extend({
       ticketType: z.literal(TicketType.REPAIR),
       repairDescription: reqString('Repair description'),
-      endDate: z.string(isReq('End date')),
     }),
-    ticketBaseZSecond.extend({
+    ticketBaseZ.extend({
       ticketType: z.literal(TicketType.DONATE),
     }),
-    ticketBaseZSecond.extend({
+    ticketBaseZ.extend({
       ticketType: z.literal(TicketType.RECEIVE),
     }),
   ],
