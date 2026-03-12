d85057b955a2e4adf31ae2440e0f39819717c612
André Ljungsvik <140394392+andreljungsvik@users.noreply.github.com>
2026-03-12 11:47:08 +0100

    fix(api): Add default region to AwsConfig

--- api/src/main/java/se/hjulverkstan/main/config/AwsConfig.java
+++ api/src/main/java/se/hjulverkstan/main/config/AwsConfig.java
@@ -1,21 +1,26 @@
 package se.hjulverkstan.main.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.sns.SnsClient;
 
 @Configuration
 public class AwsConfig {
 
+    @Value("${hjulverkstan.aws-s3.region}")
+    private String region;
+
     @Bean
     public S3Client s3Client() {
-        return S3Client.create();
+        return S3Client.builder().region(Region.of(region)).build();
     }
 
     @Bean
     public SnsClient snsClient() {
-        return SnsClient.create();
+        return SnsClient.builder().region(Region.of(region)).build();
     }
 }
 
