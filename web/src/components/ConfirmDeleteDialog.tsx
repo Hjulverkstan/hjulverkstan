98e42ccbd9ba048e127af82befe741d8faff700a
MarioGit24 <marbug0222@skola.goteborg.se>
2026-04-07 13:02:35 +0200

    feat(web): Adding message for delete dialog and a Archive button.

--- web/src/components/ConfirmDeleteDialog.tsx
+++ web/src/components/ConfirmDeleteDialog.tsx
@@ -7,11 +7,12 @@ import {
   DialogTitle,
 } from '@components/shadcn/Dialog';
 import { Button, IconButton } from '@components/shadcn/Button';
-import { Trash2 } from 'lucide-react';
+import { Trash2, Archive } from 'lucide-react';
 
 interface confirmDeleteDialogProps {
   onDelete: () => void;
   onCancel?: () => void;
+  onArchive?: () => void;
   entity: string;
   entityId?: string;
 }
@@ -21,33 +22,63 @@ export default function ConfirmDeleteDialog({
   onCancel,
   entity,
   entityId,
+  onArchive,
 }: confirmDeleteDialogProps) {
   const item = entity.toLowerCase();
   const id = entityId?.toLowerCase();
 
   return (
-    <DialogContent className="sm:max-w-[425px]">
+    <DialogContent className="max-w-[600px] p-6 text-left">
       <DialogHeader>
-        <DialogTitle>{`Delete ${item}`}</DialogTitle>
-        <DialogDescription>
-          {`You are trying to delete a ${item} ${id ? `with id ${id}.` : ''}`}
+        <DialogTitle className="mb-2 text-xl">{`Delete ${item}`}</DialogTitle>
+        <DialogDescription className="leading-relaxed text-slate-700">
+          <p className="mb-4">
+            {`You are trying to delete a ${item} ${id ? `with id ${id}.` : ''}`}
+          </p>
+
+          <p>
+            <strong>Archiving</strong> preserves all repair history and records.
+            <strong>Deleting</strong> is <strong>permanent </strong>and should
+            only be used if this {item} was created by mistake.
+          </p>
         </DialogDescription>
       </DialogHeader>
-      <DialogFooter>
+
+      <DialogFooter className="mt-8 flex flex-row items-center justify-between">
         <DialogClose asChild>
-          <Button variant="outline" type="button" onClick={onCancel}>
+          <Button
+            variant="outline"
+            type="button"
+            onClick={onCancel}
+            className={'mr-auto'}
+          >
             Cancel
           </Button>
         </DialogClose>
-        <DialogClose asChild>
-          <IconButton
-            variant={'red'}
-            type="submit"
-            onClick={onDelete}
-            icon={Trash2}
-            text="Delete"
-          />
-        </DialogClose>
+
+        <div className="flex gap-3">
+          <DialogClose asChild>
+            <IconButton
+              variant={'default'}
+              type="button"
+              onClick={onArchive}
+              icon={Archive}
+              text="Archive"
+              className="whitespace-nowrap"
+            />
+          </DialogClose>
+
+          <DialogClose asChild>
+            <IconButton
+              variant={'red'}
+              type="button"
+              onClick={onDelete}
+              icon={Trash2}
+              text="Permanent Delete"
+              className="whitespace-nowrap"
+            />
+          </DialogClose>
+        </div>
       </DialogFooter>
     </DialogContent>
   );
